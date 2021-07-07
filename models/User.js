c31391777960f357484b9deec15733ac167744e0
Ryan Blessing <ryanblessing20@yahoo.com>
2021-07-07 10:56:16 -0500

    updated hash functionality to secure user information

--- models/User.js
+++ models/User.js
@@ -1,61 +1,86 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our user model---objects user and model are moth imported from sequelize
-class User extends Model {}
+class User extends Model {
+    //set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 //define table column and configuration
-User.init(
-    {
-    //table column definitions go here
-    //define an id column
-    id:{
-        // use the special sequelize dataTypes object provide What type of data it is
-        type: DataTypes.INTEGER,
-        //equivalent to mysql's NOT NULL option
-        allowNull: false,
-        //instruct that this is primary key
-        primaryKey: true,
-        //turn on auto increment
-        autoIncrement: true
-    },
-    //define a username column
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }, 
-    //define a email column
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        //there cannot be any duplicate email values in this table
-        unique: true,
-        //if allowNull is set to false, we can run our data through validator before creating the table data
-        validate: {
-            isEmail: true
-        }
-    },
-    // define a password column
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            //this means the password must be at least four characters long
-            len: [4]
-        }
-    }
-}, {
-    //Table configuration options go here (https://sequelize.org/v5/manual/models-definition.html#configuration))
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-});
+User.init({
+            //table column definitions go here
+            //define an id column
+            id: {
+                // use the special sequelize dataTypes object provide What type of data it is
+                type: DataTypes.INTEGER,
+                //equivalent to mysql's NOT NULL option
+                allowNull: false,
+                //instruct that this is primary key
+                primaryKey: true,
+                //turn on auto increment
+                autoIncrement: true
+            },
+            //define a username column
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            //define a email column
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                //there cannot be any duplicate email values in this table
+                unique: true,
+                //if allowNull is set to false, we can run our data through validator before creating the table data
+                validate: {
+                    isEmail: true
+                }
+            },
+            // define a password column
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    //this means the password must be at least four characters long
+                    len: [4]
+                }
+            }
+        }, {
+            //where we add our hooks to secure the bcrypt password hook function
+            hooks: {
+                //set up beforeCreate lifecycle "hook" functionality
+                // beforeCreate(userData) {
+                //     return bcrypt.hash(userData.password, 10).then(newUserData => {
+                //         return newUserData
+                //     });
+                // }
 
-module.exports = User;
+                //same function as above but easier to read!
+                    async beforeCreate(newUserData) {
+                        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                        return newUserData;
+                    },
+                    //set up beforeUpdate lifecycle "hook" functionality--- allows user to update there password- must add option to quey in router.put for it to work!
+                    async beforeUpdate(updatedUserData) {
+                        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                        return updatedUserDatal
+                    }
+            },
+                //Table configuration options go here (https://sequelize.org/v5/manual/models-definition.html#configuration))
+                sequelize,
+                // don't automatically create createdAt/updatedAt timestamp fields
+                timestamps: false,
+                // don't pluralize name of database table
+                freezeTableName: true,
+                // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+                underscored: true,
+                // make it so our model name stays lowercase in the database
+                modelName: 'user'
+            }
+        );
 
+        module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,53 +1,91 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const {User} = require('../../models');
 
 //GET/api/users
 router.get('/', (request, response) => {
     //access our User Model and run .findAll() method)
     User.findAll({
-        attributes: { exclude: ['password']}
-    })
-    .then(dbUserData => response.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        response.status(500).json(err);
-    });
+            attributes: {
+                exclude: ['password']
+            }
+        })
+        .then(dbUserData => response.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            response.status(500).json(err);
+        });
 });;
 
 //GET/api/user/1
 router.get('/:id', (request, response) => {
     User.findOne({
-        attributes: { exclude: [ 'password']},
-        where: {
-            id: request.params.id
-        }
-    })
-    .then(dbUserData => {
-        if(!dbUserData) {
-            response.status(404).json({ message: 'No User found with this ID' });
-            return;
-        }
-        response.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        response.status(500).json(err);
-    });
+            attributes: {
+                exclude: ['password']
+            },
+            where: {
+                id: request.params.id
+            }
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                response.status(404).json({
+                    message: 'No User found with this ID'
+                });
+                return;
+            }
+            response.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            response.status(500).json(err);
+        });
 });
 
 //Post/api/user
 router.post('/', (request, response) => {
     //expects username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'
     User.create({
-        username: request.body.username,
-        email: request.body.email,
-        password: request.body.password
-    })
-    .then(dbUserData => response.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        response.status(500).json(err);
-    });
+            username: request.body.username, //'Lernantino',
+            email: request.body.email, //'lernantino@gmail.com', 
+            password: request.body.password //'password1234' 
+        })
+        .then(dbUserData => response.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            response.status(500).json(err);
+        });
+});
+
+router.post('/login', (request, response) => {
+    // Query Operation
+    //expects username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'
+    User.findOne({
+            where: {
+                email: request.body.email
+            }
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                response.status(400).json({
+                    message: 'No user with that email address!'
+                });
+                return;
+            }
+            //Verify User
+            const validPassword = dbUserData.checkPassword(request.body.password);
+            //this returns a boolean so this additional statement below is needed to handle either true or false
+            if (!validPassword) {
+                response.status(400).json({
+                    message: 'Incorrect password!'
+                });
+                return;
+            }
+
+            response.json({
+                user: dbUserData,
+                message: ' You are now logged in!'
+            });
+        });
 });
 
 //Put/api/user/1
@@ -56,40 +94,46 @@ router.put('/:1', (request, response) => {
 
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(request.body, {
-        where: {
-            id: request.params.id
-        }
-    })
-    .then(dbUserData => {
-        if(!dbUserData[0]) {
-            response.status(404).json({ message: 'No User Found With This Id' });
-            return;
-        }
-        response.json(dbUserData)
-    })
-    .catch(err => {
-        console.log(err);
-        response.status(500).json(err);
-    });
+            //updated to use to updated hook!
+            individualHooks: true,
+            where: {
+                id: request.params.id
+            }
+        })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                response.status(404).json({
+                    message: 'No User Found With This Id'
+                });
+                return;
+            }
+            response.json(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            response.status(500).json(err);
+        });
 });
 
 //Delete/api/user/1
 router.delete('/:1', (request, response) => {
     User.destroy({
-        where: {
-            id: request.params.id
-        }
-    })
-    .then(dbUserData => {
-        if(!dbUserData) {
-            response.status(404).json({ message: 'No user found with this ID' })
-        }
-        response.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        response.status(500).json(err);
-    });
+            where: {
+                id: request.params.id
+            }
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                response.status(404).json({
+                    message: 'No user found with this ID'
+                })
+            }
+            response.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            response.status(500).json(err);
+        });
 });
 
 module.exports = router;
