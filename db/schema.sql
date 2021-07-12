4d6aabe3dc712e79b344c3fbf607d39933899322
Ryan Blessing <ryanblessing20@yahoo.com>
2021-07-12 11:33:43 -0500

    u[dated

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
 DROP DATABASE IF EXIST just_tech_news_db;
 
 CREATE DATABASE just_tech_news_db;
+
+INSERT INTO post (title, post_url, user_id, created_at, updated_at)
+VALUES
+ ("Taskmaster goes public!", "https://taskmaster/press", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
