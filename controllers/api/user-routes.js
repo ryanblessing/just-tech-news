73667572a8a7c7b7391f2cc3410563d9ec16bdb6
Ryan Blessing <ryanblessing20@yahoo.com>
2021-07-14 10:35:26 -0500

    cookies session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
+const {
+  User,
+  Post,
+  Comment,
+  Vote
+} = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
   User.findAll({
-    attributes: { exclude: ['password'] }
-  })
+      attributes: {
+        exclude: ['password']
+      }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -15,34 +22,37 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
+      attributes: {
+        exclude: ['password']
       },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
+      where: {
+        id: req.params.id
+      },
+      include: [{
           model: Post,
-          attributes: ['title']
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
         }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
-      }
-    ]
-  })
+      ]
+    })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({
+          message: 'No user found with this id'
+        });
         return;
       }
       res.json(dbUserData);
@@ -56,14 +66,16 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+    .then(dbUserData => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
     });
 });
 
@@ -75,18 +87,32 @@ router.post('/login', (req, res) => {
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({
+        message: 'No user with that email address!'
+      });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res.status(400).json({
+        message: 'Incorrect password!'
+      });
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    req.session.save(() => {
+      //declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({
+        user: dbUserData,
+        message: 'You are now logged in!'
+      });
+    })
   });
 });
 
@@ -95,14 +121,16 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
     .then(dbUserData => {
       if (!dbUserData[0]) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({
+          message: 'No user found with this id'
+        });
         return;
       }
       res.json(dbUserData);
@@ -115,13 +143,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   User.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
+      where: {
+        id: req.params.id
+      }
+    })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({
+          message: 'No user found with this id'
+        });
         return;
       }
       res.json(dbUserData);
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 const hbs = exphbs.create({});
 
@@ -17,6 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
