b564ad542702fc5a294b94aba512e4a6ea08552f
Ryan Blessing <ryanblessing20@yahoo.com>
2021-07-15 20:57:07 -0500

    khfb::

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,8 +22,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
